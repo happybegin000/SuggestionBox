50a3c78984479041727c0590422c76e4511dc26f
happybegin000 <914013737@qq.com>
2017-01-26 10:53:16 +0800

    Add files via upload

---
+++ suggestions.js
@@ -0,0 +1,21 @@
+app.factory('suggestions',function(){
+	var demoSuggestions={
+		posts:[
+		{
+			title:'Free Pizza',
+			upvotes:15,
+			comments:[],
+		},{
+			title:'more jokes',
+			upvotes:9,
+			comments:[],
+		},{
+			title:'more free water',
+			upvotes:20,
+			comments:[],
+		}
+
+		]
+	};
+	return demoSuggestions;
+});
